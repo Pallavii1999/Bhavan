64f92a6ab5fa00414fe03dc1b71fd0d65daa8138
Pallavii1999 <pallupowar31@gmail.com>
2025-07-24 13:53:57 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using OfficeOpenXml;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// ? Set EPPlus license (before any EPPlus usage)
+ExcelPackage.License.SetNonCommercialPersonal("Pallavi");
+
+// Configure middleware
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
